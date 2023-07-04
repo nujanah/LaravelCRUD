67cb9966b930ca6ff2f2c6f5a9ff01d865cbdfba
nujanah <nurjanahdwianggraini13@gmail.com>
2023-07-04 12:21:06 +0700

    Initial commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,192 @@
+{{-- <!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="index.css">
+  <link rel="stylesheet" type="text/css" href="fontawesome/css/all.min.css">
+
+  <title>Company Create</title>
+</head>
+
+<body>
+
+  <!-- Jumbotron -->
+  <div class="jumbotron jumbotron-fluid text-center">
+    <div class="container">
+      <h1 class="display-4"><span class="font-weight-bold">Create Company</span></h1>
+      <hr>
+      <p class="lead font-weight-bold">Semoga Sukses<br>
+        Enjoy Your Bussines</p>
+    </div>
+  </div>
+  <!-- Akhir Jumbotron -->
+
+  <!-- Navbar -->
+  <x-slot name="header">
+    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        {{ __('Dashboard') }}
+    </h2>
+    
+</x-slot>
+  <nav class="navbar navbar-expand-lg  bg-dark">
+    <div class="container">
+      <a class="navbar-brand text-white" href="user.php"><strong>Comp</strong> WORLD</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link mr-4" href="./companies">Langsung Masuk</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- Akhir Navbar -->
+
+  <!-- Menu -->
+  <div class="container">
+
+    <div class="row mb-5 mt-5 ">
+
+    </div>
+  </div>
+  <!-- Akhir Menu -->
+
+  <!-- Awal Footer -->
+  <hr class="footer">
+  <div class="container">
+    <div class="row footer-body">
+      <div class="col-md-6">
+        <div class="copyright">
+          <strong>Copyright</strong> <i class="far fa-copyright"></i> 2023 - Designed by Dwi & Zidan</p>
+        </div>
+      </div>
+
+      <div class="col-md-6 d-flex justify-content-end">
+      </div>
+    </div>
+  </div>
+  <!-- Akhir Footer -->
+
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="js/jquery.js"></script>
+</body>
+
+</html> --}}
+
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="index.css">
+  <link rel="stylesheet" type="text/css" href="fontawesome/css/all.min.css">
+
+  <title>Company Create</title>
+</head>
+
+<body>
+  <x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+    <div class="jumbotron jumbotron-fluid text-center">
+        <div class="container">
+          <h1 class="display-4"><span class="font-weight-bold"><b>PUSKESMAS</b></span></h1>
+          <p class="lead font-weight-bold">Enjoy Your Life</p>
+            <a class="btn btn-primary btn-lg" href="./companies" role="button">MASUK</a>
+        </div>
+      </div>
+</x-app-layout>
+  <!-- Jumbotron -->
+
+
+
+
+  <!-- Akhir Jumbotron -->
+
+  <!-- Navbar -->
+  {{-- <x-slot name="header">
+    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        {{ __('Dashboard') }}
+    </h2>
+    
+</x-slot>
+  <nav class="navbar navbar-expand-lg  bg-dark">
+    <div class="container">
+      <a class="navbar-brand text-white" href="user.php"><strong>Comp</strong> WORLD</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link mr-4" href="./companies">Langsung Masuk</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav> --}}
+  <!-- Akhir Navbar -->
+
+  <!-- Menu -->
+  {{-- <div class="container">
+
+    <div class="row mb-5 mt-5 ">
+
+    </div>
+  </div> --}}
+  <!-- Akhir Menu -->
+
+  <!-- Awal Footer -->
+  <hr class="footer">
+  <div class="container">
+    <div class="row footer-body">
+      <div class="col-md-6">
+        <div class="copyright">
+          <strong>Copyright</strong> <i class="far fa-copyright"></i> 2023 - Designed by Nurjanah Dwi Anggraini</p>
+        </div>
+      </div>
+
+      <div class="col-md-6 d-flex justify-content-end">
+      </div>
+    </div>
+  </div>
+  <!-- Akhir Footer -->
+
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="js/jquery.js"></script>
+</body>
+
+</html>
